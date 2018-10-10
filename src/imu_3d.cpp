3a41e828fcd51da7530f5c2c10880782c49dbbdb
James Jackson <superjax08@gmail.com>
2018-10-10 17:53:52 -0600

    multiwindow built, but not working diverges

--- src/imu_3d.cpp
+++ src/imu_3d.cpp
@@ -232,3 +232,119 @@ TEST(Imu3D, SingleWindow)
 //    cout << "e " << error << endl;
     EXPECT_LE(error, 0.1);
 }
+
+TEST(Imu3D, MultiWindow)
+{
+    Simulator multirotor(false);
+    multirotor.load("../lib/multirotor_sim/params/sim_params.yaml");
+
+    const int N = 10;
+
+    Vector6d b, bhat;
+    b.block<3,1>(0,0) = multirotor.get_accel_bias();
+    b.block<3,1>(3,0) = multirotor.get_gyro_bias();
+    bhat.setZero();
+    bhat = b;
+
+    Problem problem;
+
+    Eigen::Matrix<double, 7, N+1> xhat, x;
+    Eigen::Matrix<double, 3, N+1> vhat, v;
+
+    xhat.col(0) = multirotor.get_pose().arr_;
+    vhat.col(0) = multirotor.dyn_.get_state().segment<3>(dynamics::VX);
+    x.col(0) = multirotor.get_pose().arr_;
+    v.col(0) = multirotor.dyn_.get_state().segment<3>(dynamics::VX);
+
+    // Anchor origin node (pose and velocity)
+    problem.AddParameterBlock(xhat.data(), 7, new XformAutoDiffParameterization());
+    problem.SetParameterBlockConstant(xhat.data());
+    problem.AddParameterBlock(vhat.data(), 3);
+    problem.SetParameterBlockConstant(vhat.data());
+
+    // Declare the bias parameters
+    problem.AddParameterBlock(bhat.data(), 6);
+
+    std::vector<Simulator::measurement_t, Eigen::aligned_allocator<Simulator::measurement_t>> meas_list;
+    multirotor.get_measurements(meas_list);
+
+    std::vector<Imu3DFactorCostFunction*> factors;
+    factors.push_back(new Imu3DFactorCostFunction(0, bhat, multirotor.get_imu_noise_covariance()));
+
+    // Integrate for N frames
+    int node = 0;
+    Imu3DFactorCostFunction* factor = factors[node];
+    while (node < N)
+    {
+        multirotor.run();
+        factor->integrate(multirotor.t_, multirotor.get_imu_prev());
+        multirotor.get_measurements(meas_list);
+        bool new_node = false;
+        for (auto it = meas_list.begin(); it != meas_list.end(); it++)
+        {
+            switch(it->type)
+            {
+            case Simulator::FEAT:
+                // simulate a camera measurement
+                new_node = true;
+                break;
+            default:
+                break;
+            }
+        }
+
+        if (new_node)
+        {
+            node += 1;
+
+            // estimate next node pose and velocity with IMU preintegration
+            factor->estimate_xj(xhat.data()+7*(node-1), vhat.data()+3*(node-1), xhat.data()+7*(node), vhat.data()+3*(node));
+            // Calculate the Information Matrix of the IMU factor
+            factor->finished();
+
+            // Save off True Pose and Velocity for Comparison
+            x.col(node) = multirotor.get_pose().arr_;
+            v.col(node) = multirotor.dyn_.get_state().segment<3>(dynamics::VX);
+
+            // Declare the new parameters used for this new node
+            problem.AddParameterBlock(xhat.data()+7*node, 7, new XformAutoDiffParameterization());
+            problem.AddParameterBlock(vhat.data()+3*node, 3);
+
+            // Add IMU factor to graph
+            problem.AddResidualBlock(new Imu3DFactorAutoDiff(factor), NULL, xhat.data()+7*(node-1), xhat.data()+7*node, vhat.data()+3*(node-1), vhat.data()+3*node, bhat.data());
+
+            // Start a new Factor
+            factors.push_back(new Imu3DFactorCostFunction(multirotor.t_, bhat, multirotor.get_imu_noise_covariance()));
+            factor = factors[node];
+        }
+    }
+
+
+
+
+    Matrix6d P = Matrix6d::Identity() * 0.1;
+    problem.AddResidualBlock(new XformNodeFactorAutoDiff(new XformNodeFactorCostFunction(x.col(1), P)), NULL, xhat.data()+7);
+
+
+    Solver::Options options;
+    options.max_num_iterations = 100;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+    Solver::Summary summary;
+
+    cout.flush();
+
+    cout << "xhat0\n" << xhat.transpose() << endl;
+    cout << "bhat0\n" << bhat.transpose() << endl;
+
+    ceres::Solve(options, &problem, &summary);
+    double error = (b - bhat).norm();
+
+    //    summary.FullReport();
+    cout << "x\n" << x.transpose() << endl;
+    cout << "xhat0\n" << xhat.transpose() << endl;
+    cout << "b\n" << b.transpose() << endl;
+    cout << "bhat\n" << bhat.transpose() << endl;
+    cout << "e " << error << endl;
+    EXPECT_LE(error, 0.1);
+}
